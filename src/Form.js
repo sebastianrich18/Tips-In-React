e15ff97f83f39a8fc45eea7b0599b7b41a9f5a84
Sebastian <sebastianrichel18@gmail.com>
2022-03-24 00:16:13 -0400

    works

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,7 @@ import { Employee } from "./Employee";
 
 export function Form(props) {
 
-    const [total, setTotal] = React.useState(0);
+    const [total, setTotal] = React.useState("0");
     const [pays, setPays] = React.useState([]);
     const [hours, setHours] = React.useState([]);
     
@@ -13,27 +13,21 @@ export function Form(props) {
         for(let key in hours) {
             totalHours += parseFloat(hours[key]);
         }
-        
-        console.log(hours.entries())
+
+        let newPays = [...pays];
         for (let i in hours) {
             let newPay = ((parseFloat(hours[i]) / totalHours) * parseFloat(total)).toFixed(2);
             if (newPay) {
-                console.log(newPay, i)
-                let newPays = pays;
                 newPays[i] = newPay;
-                setPays(newPays);
             }
-            
         }
-        console.log(pays)
+        setPays(newPays);
     }
     
     const handleHoursChange = (event) => {
-        let newHours = hours;
-        hours[event.target.name] = event.target.value;
+        let newHours = [...hours];
+        newHours[event.target.name] = event.target.value;
         setHours(newHours);
-        console.log(hours)
-
     }
 
     const handleTotalTipsChange = (event) => {
@@ -43,6 +37,7 @@ export function Form(props) {
 
     return (
         <div className="form">
+            {console.log(pays)}
             <form onSubmit={handleSubmit}>
                 {Array.from({ length: props.num }, (v, i) => <Employee index={i} key={i} onHoursChange={handleHoursChange} pay={pays[i]} />)}
                 <input type="number" min={0} name="total" placeholder="Total Tips" onChange={handleTotalTipsChange} />
